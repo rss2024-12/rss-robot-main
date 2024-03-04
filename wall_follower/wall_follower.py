ce2ebf6b1f1f2d0cb89b01b081fe4bc7b7922665
heathnilsen <hnilsen@mit.edu>
2024-03-04 21:48:56 +0000

    Heath initial commit

--- wall_follower/wall_follower.py
+++ wall_follower/wall_follower.py
@@ -1,186 +1,243 @@
 #!/usr/bin/env python3
 import numpy as np
 import rclpy
-import random
+import time
+import math
 from rclpy.node import Node
 from sensor_msgs.msg import LaserScan
 from ackermann_msgs.msg import AckermannDriveStamped
+from std_msgs.msg import Float32
 from visualization_msgs.msg import Marker
 
-
-from wall_follower.visualization_tools import VisualizationTools
+from wall_follower.visualization_tools import *
 
 
 class WallFollower(Node):
 
+    WALL_TOPIC = "/wall"
+    FRONT_WALL_TOPIC ="/front_wall"
+    BACK_WALL_TOPIC = "/back_wall"
+    WALL_OFFSET_TOPIC = "/wall_offset"
+    TURN_RADIUS = 1
+
     def __init__(self):
         super().__init__("wall_follower")
-        # Declare parameters to make them available for use
+        # Declaring parameters to make them available for use
         self.declare_parameter("scan_topic", "default")
         self.declare_parameter("drive_topic", "default")
         self.declare_parameter("side", "default")
         self.declare_parameter("velocity", "default")
         self.declare_parameter("desired_distance", "default")
-        self.declare_parameter('wall_topic', '/wall')
-        self.declare_parameter('safety_drive_topic', '/follow_drive')
 
         # Fetch constants from the ROS parameter server
         self.SCAN_TOPIC = self.get_parameter('scan_topic').get_parameter_value().string_value
         self.DRIVE_TOPIC = self.get_parameter('drive_topic').get_parameter_value().string_value
-        self.SAFETY_DRIVE_TOPIC = self.get_parameter('safety_drive_topic').get_parameter_value().string_value
         self.SIDE = self.get_parameter('side').get_parameter_value().integer_value
         self.VELOCITY = self.get_parameter('velocity').get_parameter_value().double_value
         self.DESIRED_DISTANCE = self.get_parameter('desired_distance').get_parameter_value().double_value
-        self.WALL_TOPIC = self.get_parameter('wall_topic').get_parameter_value().string_value 
-
-        # Based on the right or left side, the car will only 
-        # account for scans in the cone between these angles
-        # side_min_angle : Radians, set >= 0
-        # side_max_angle : Radians, set <= 135 
-        self.side_min_angle = -45* (np.pi/180)
-        self.side_max_angle = 135 * (np.pi/180)
-
-        # Another parameter you can set is the distance at which a scan is disregarded (Look-Ahead Distance) 
-        # max_scan_distance : meters
-        self.max_scan_distance = 3*self.DESIRED_DISTANCE
-
-        # For Visualization
-        self.visualize_line = True
+		
+	    # Initialize publishers and subscribers
+        self.drive_publisher = self.create_publisher(AckermannDriveStamped, self.DRIVE_TOPIC, 10)
+        self.wall_publisher = self.create_publisher(Marker, self.WALL_TOPIC, 1)
+        self.front_wall_publisher = self.create_publisher(Marker, self.FRONT_WALL_TOPIC, 1)
+        self.back_wall_publisher = self.create_publisher(Marker, self.BACK_WALL_TOPIC, 1)
+        self.wall_offset_publisher = self.create_publisher( Float32, self.WALL_OFFSET_TOPIC, 10)
         
-        # Needed for PID
-        self.prev_dist_error = 0
-        self.prev_angle_error = 0
-        self.prev_time = 0
-    
-        # Publishers
-        self.wall_visual_publisher = self.create_publisher(Marker, self.WALL_TOPIC, 10)
-        self.drive_input_publisher = self.create_publisher(AckermannDriveStamped, self.SAFETY_DRIVE_TOPIC, 10) # Set to self.DRIVE_TOPIC to circumvent the safety controller
+        self.scan_subscription = self.create_subscription(LaserScan, self.SCAN_TOPIC, self.scan_callback, 10)
 
-        # Subscribtion 
-        self.create_subscription(LaserScan, self.SCAN_TOPIC, self.laser_callback, 10)
+        self.last_angle = 0
+        self.last_error = 0
+        self.integrated_error = 0
 
-    
-    # Subscription Callback Function (Scan Processing, Feed Errors into Controller, & Send Drive Command)
-    def laser_callback(self, laser_scan):
+
+    def scan_callback(self, scan_msg):
         self.SIDE = self.get_parameter('side').get_parameter_value().integer_value
         self.VELOCITY = self.get_parameter('velocity').get_parameter_value().double_value
         self.DESIRED_DISTANCE = self.get_parameter('desired_distance').get_parameter_value().double_value
-        current_time = (laser_scan.header.stamp.nanosec / 1e9)
-        # self.get_logger().info(str(self.VELOCITY))
-        num_points = len(laser_scan.ranges)
-        distances = np.array(laser_scan.ranges)
-        angles = np.linspace(laser_scan.angle_min, laser_scan.angle_max, num_points)
-        scan_vectors = np.vstack((distances, angles))
         
-        # Filter Points based on Side & Angles
-        if self.SIDE == 1:
-            scan_vectors = scan_vectors[:, (scan_vectors[1,:]<= self.side_max_angle) & (scan_vectors[1,:]>= self.side_min_angle)]
-        else:
-            scan_vectors = scan_vectors[:, (-scan_vectors[1,:] <= self.side_max_angle) & (-scan_vectors[1,:] >= self.side_min_angle)]
+        # Computing relevant values and arrays for rest of method
+        sample_size = len(scan_msg.ranges)
+        front_index = sample_size//2
+        view_range_start = scan_msg.angle_min
+        view_range_end = scan_msg.angle_max
+        view_range = view_range_end - view_range_start
+        angles = np.linspace(view_range_start, view_range_end, sample_size)
         
-        # Filter Points based on Look-Ahead Distance
-        scan_vectors = scan_vectors[:, scan_vectors[0, :] <= self.max_scan_distance]
+        # Creating scan data array to be given to 'relevat_samples'
+        # TODO: this can be changed, including the revelevant samples method
+        scan_data = np.array([angles, scan_msg.ranges])
+
+        # Use the relevant samples to gather relevant data for the front, back and specified side
+        (relevant_data_polar, relevant_data_front_polar, relevant_data_back_polar) = self.relevant_samples(scan_data, self.SIDE)
+
+        # USED DURING TESTING
+        # if not relevant_data_polar.any():
+        #     pass
+
+        # Converting to cartesian and finding linear fit
+        relevant_data_cartesian = self.polar_to_cartesian(relevant_data_polar)
+        wall_m, wall_y_offset = self.linear_fit(relevant_data_cartesian)
+
+        # Creating lines for visualization
+        x_viz = np.linspace(-2., 2., num=20)
+        y_viz = wall_m*x_viz + wall_y_offset
+        VisualizationTools.plot_line(x_viz, y_viz, self.wall_publisher, frame="/laser")
+
+        # Checking to see if the front or back of the car are 'good'
+        front_good = False
+        back_good = False
+        if len(relevant_data_front_polar[1]) > 2:
             
-        # Find X,Y points (+Visualize), Gather PID output, Feed to Command Input
-        if scan_vectors.size == 0:
-            output_steering_angle = 0
-            velocity = self.VELOCITY
-        else:
-            xs = scan_vectors[0,:]*np.cos(scan_vectors[1,:])
-            ys = scan_vectors[0,:]*np.sin(scan_vectors[1,:])
+            front_good = True
+            relevant_data_front_cartesian = self.polar_to_cartesian(relevant_data_front_polar)
+            front_wall_m, front_wall_y_offset = self.linear_fit(relevant_data_front_cartesian)
 
-            best_model, best_inliers = self.ransac(xs, ys, sample_size=len(xs))
+            front_x_wall = np.linspace(1., 4., num=16)
+            front_y_wall = front_wall_m*front_x_wall + front_wall_y_offset
 
-            # Extracting slope and intercept from the best model
-            slope, intercept = best_model
-            # slope, intercept = np.polyfit(xs, ys, 1)
+            VisualizationTools.plot_line(front_x_wall, front_y_wall, self.front_wall_publisher, frame="/laser")
 
-            if self.SIDE == 1:
-                intercept  -= self.DESIRED_DISTANCE
-            else: 
-                intercept += self.DESIRED_DISTANCE
+        if len(relevant_data_back_polar[1]) > 2:
             
-            # Visualize
-            if self.visualize_line:
-                x = xs
-                y = np.array([slope * xi + intercept for xi in x])
-                VisualizationTools.plot_line(x, y, self.wall_visual_publisher, frame="/laser")
+            back_good = True
+            relevant_data_back_cartesian = self.polar_to_cartesian(relevant_data_back_polar)
+            back_wall_m, back_wall_y_offset = self.linear_fit(relevant_data_back_cartesian)
 
-            errors = [intercept,slope]
+            back_x_wall = np.linspace(-4., -1., num=16)
+            back_y_wall = back_wall_m*back_x_wall + back_wall_y_offset
 
-            #PID control
-            output_steering_angle = self.PID_controler(errors, current_time)
-            velocity = self.VELOCITY * max(0.5, 0.1 + output_steering_angle**2)
-            
-        msg = AckermannDriveStamped()
-        msg.drive.speed = velocity 
-        msg.drive.steering_angle = float(output_steering_angle)
-        self.drive_input_publisher.publish(msg)
-
-    def PID_controler(self, error, curr_time):
-
-        # Assuming error is distance to wall
-        # error[0] is distance, error[1] is slope
-        Kp_dist = 0.5
-        Kd_dist = 0.1
-        Ki_dist = 0
-
-        Kp_slope = 1
-        Kd_slope = 0.3
-        Ki_slope = 0
-
-        dt = curr_time - self.prev_time
-        de_dist = error[0] - self.prev_dist_error
-        de_slope = error[1] - self.prev_angle_error
-        de_dt_dist = de_dist/dt
-        de_dt_slope = de_slope/dt
-
-        self.prev_time = curr_time
-        self.prev_dist_error = error[0]
-        self.prev_angle_error = error[1]
-
-        return_value = (Kp_dist*error[0] + Kp_slope*error[1] + Kd_dist*de_dt_dist + Kd_slope*de_dt_slope)/2
-        if return_value < -0.34:
-            return_value = -0.34
-        if return_value > 0.34:
-            return_value = 0.34
+            VisualizationTools.plot_line(back_x_wall, back_y_wall, self.back_wall_publisher, frame="/laser")
         
-        # self.get_logger().info(f'PD: {Kp_dist*error[0]} PS:{Kp_slope*error[1]} DD:{Kd_dist*de_dt_dist} DS:{Kd_slope*de_dt_slope}')
-        # self.get_logger().info(f'{return_value}')
-        return (Kp_dist*error[0] + Kp_slope*error[1] + Kd_dist*de_dt_dist + Kd_slope*de_dt_slope)/2
 
+        wall_offset = wall_y_offset + math.atan(wall_m)
+        # self.get_logger().info('Wall offset: %s = %s + %s' % (wall_offset,  wall_y_offset, math.atan(wall_m)))
+        
+        Tc = 1.2
+        Kp = 0.2
+        Ki = 2*Kp/Tc    *0
+        Kd = Kp*Tc/8      *0
+
+        wall_offset_error = wall_offset - self.DESIRED_DISTANCE*self.SIDE
+        wall_offset_error_dt = (wall_offset_error - self.last_error)
+        self.integrated_error += wall_offset_error
+
+        wheel_angle = wall_offset_error*Kp + wall_offset_error_dt*Kd + Ki*self.integrated_error
+
+        # wheel_angle = 100*(wall_y_offset - self.DESIRED_DISTANCE*self.SIDE)
+
+        # # checking to see if any of the ranges are less than the turn radius plus the desired distance
+        # range_checker = False
+        # for i in range(front_index-5, front_index+5):
+        #     if scan_msg.ranges[i] < self.TURN_RADIUS + self.DESIRED_DISTANCE:
+        #         checker = True
+
+        # checker_side_1 = []
+        # for s in scan_msg.ranges[front_index:(front_index*3)//4]:
+        #     checker_side_1.append(s < 1.5*(self.TURN_RADIUS + self.DESIRED_DISTANCE))
+
+        # checker_side_minus_1 = []
+        # for s in scan_msg.ranges[front_index//4:front_index]:
+        #     checker_side_minus_1.append(s < 1.5*(self.TURN_RADIUS + self.DESIRED_DISTANCE))
+
+        # # Replaces If statment
+        # if range_checker:
+        #     if (self.SIDE == 1 and all(checker_side_1)) or (self.SIDE == -1 and all(checker_side_minus_1)):
+        #         wheel_angle = -100.0 * self.SIDE
+        #         self.integrated_error = 0
+
+        
+        # Original If statement
+        if any([scan_msg.ranges[i] < self.TURN_RADIUS + self.DESIRED_DISTANCE for i in range(front_index-5, front_index+5)]):
+            if self.SIDE == 1 and all([s < (self.TURN_RADIUS + self.DESIRED_DISTANCE)*1.5 for s in scan_msg.ranges[front_index:(front_index * 3)//4]]) or self.SIDE == -1 and all([s < (self.TURN_RADIUS + self.DESIRED_DISTANCE)*1.5 for s in scan_msg.ranges[front_index//4:front_index]]):
+                wheel_angle = -100.0 * self.SIDE
+                self.integrated_error = 0
+        
+        drive_msg = AckermannDriveStamped()
+        # drive_msg.header.stamp = self.get_clock().now().to_msg()
+        # drive_msg.header.frame_id = 'base_link'
+        drive_msg.drive.steering_angle = wheel_angle
+        # drive_msg.drive.steering_angle_velocity = float
+        drive_msg.drive.speed = self.VELOCITY #* (0.75 + 0.25 * 1/(math.sqrt(abs(wheel_angle)+1)))
+        # drive_msg.drive.acceleration = float
+        # drive_msg.drive.jerk = float
+
+        self.drive_publisher.publish(drive_msg)
+
+        wall_offset_msg = Float32()
+        wall_offset_msg.data = wall_offset_error
+        self.wall_offset_publisher.publish(wall_offset_msg)
+
+        self.last_error = wall_offset_error
+
+    # Given data that includes x and y coordinates, generate a slope and y-intercept for line fit
+    def linear_fit(self, data):
+        x = data[0]
+        y = data[1]
+        A = np.vstack([x, np.ones(len(x))]).T
+        m, b = np.linalg.lstsq(A, y, rcond=None)[0]
+        return m, b
     
-    def ransac(self,X, y, n_iters=100, sample_size=20, inlier_threshold=1.0):
-        best_model = None
-        best_inliers = None
-        max_inliers = 0
+    # Given data that includes polar coordinates, convert them to cartesian 
+    def polar_to_cartesian(self, data):
+        theta = data[0]
+        r = data[1]
+        x = r*np.cos(theta)
+        y = r*np.sin(theta)
+        return np.array([x, y])
+    
+    # Given the scan data and side to follow, return relevant samples of the side as well as in front and behind
+    def relevant_samples(self, samples, side):
+        samples_pairwise = samples.T
+        # self.get_logger().info('samples_pairwise: "%s"' % samples_pairwise)
 
-        for _ in range(n_iters):
-            # Step 1: Randomly sample a subset of points
-            random_indices = random.sample(range(len(X)), sample_size)
-            X_sampled = X[random_indices]
-            y_sampled = y[random_indices]
+        # here we are 
+        if side == 1:
+            # self.get_logger().info('taking LHS samples for self.SIDE == 1 == "%s"' % (self.SIDE,))
+            out_samples = samples_pairwise[len(samples_pairwise)//2:]
+        elif side == -1:
+            # self.get_logger().info('taking RHS samples for self.SIDE == -1 == "%s"' % (self.SIDE,))
+            out_samples = samples_pairwise[:len(samples_pairwise)//2]
+        else:
+            # self.get_logger().info('taking MID samples for self.SIDE == 0 == "%s"' % (self.SIDE,))
+            out_samples = samples_pairwise[2*len(samples_pairwise)//5:3*len(samples_pairwise)//5]
 
-            # Step 2: Fit a model to the sampled points
-            model = np.polyfit(X_sampled, y_sampled, 1)  # Linear regression model, adjust degree as needed
+        # original code
+        closest_distance = (min([s[1] for s in out_samples]))
+        furthest_distance_to_consider = (closest_distance +  self.DESIRED_DISTANCE)*2
+        out_samples = np.array([s for s in out_samples if s[1] <= furthest_distance_to_consider])
+        closest_distance_index = np.argmin(out_samples.T[1])
 
-            # Step 3: Calculate inliers based on the model
-            residuals = np.abs(y - np.polyval(model, X))
-            inliers = np.where(residuals < inlier_threshold)[0]
+        # # finding closest distance
+        # distances = []
+        # for i in out_samples:
+        #     distances.append(i[1])
+        # closest_distance = min(distances)
 
-            # Step 4: Check if the current model has more inliers
-            if len(inliers) > max_inliers:
-                max_inliers = len(inliers)
-                best_model = model
-                best_inliers = inliers
+        # # determining the furthest distance to consider
+        # furthest_distance_to_consider = (closest_distance+self.DESIRED_DISTANCE)*2
 
-        return best_model, best_inliers
+        # # finding the distances that are less than the furthest distance just calculated
+        # out_samples = []
+        # for i in out_samples:
+        #     if i[1] <= furthest_distance_to_consider:
+        #         out_samples.append(i)
+        # out_samples = np.array(out_samples)
 
+        # closest_distance_index = np.argmin(out_samples.T[1])
+        
+        # determining the front and back samples based on the specified side
+        if side == 1:
+            out_samples_front = out_samples[:closest_distance_index]
+            out_samples_back = out_samples[closest_distance_index:]
+        elif side == -1:
+            out_samples_front = out_samples[closest_distance_index:]
+            out_samples_back = out_samples[:closest_distance_index]
 
-
+        
+        return (out_samples.T, out_samples_front.T, out_samples_back.T)
+        
 
 def main():
-    
     rclpy.init()
     wall_follower = WallFollower()
     rclpy.spin(wall_follower)
@@ -191,4 +248,3 @@ def main():
 if __name__ == '__main__':
     main()
     
- 
